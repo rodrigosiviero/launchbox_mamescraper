46ef2231372220ee8dabb4a93f78c0dec9b6077d
unknown <rodrigosiviero at gmail.com>
2017-05-14 14:37:56 -0300

    Initial Commit

---
+++ MameVideoScraper/MameVideoSnaps.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+using Unbroken.LaunchBox.Plugins;
+using Unbroken.LaunchBox.Plugins.Data;
+using System.Net;
+using System.IO;
+using System.Reflection;
+using System;
+
+namespace MameVideoScraper
+{
+    public class MameVideoSnaps : IGameMenuItemPlugin
+    {
+        public static string getplataforma { get; set; }
+        public static string getgame { get; set; }
+        public static string progetto = "http://www.progettosnaps.net/videosnaps/mp4";
+        public bool SupportsMultipleGames
+        {
+            get { return true; }
+        }
+
+        public string Caption
+        {
+            get { return "Mame Snaps Scraper"; }
+        }
+
+        public System.Drawing.Image IconImage
+        {
+            get { return null; }
+        }
+
+        public bool ShowInLaunchBox
+        {
+            get { return true; }
+        }
+
+        public bool ShowInBigBox
+        {
+            get { return false; }
+        }
+
+        public static object Configs { get; private set; }
+
+        public bool GetIsValidForGame(IGame selectedGame)
+        {
+            return !string.IsNullOrEmpty(selectedGame.ApplicationPath);
+        }
+
+        public bool GetIsValidForGames(IGame[] selectedGames)
+        {
+            return selectedGames.Any(g => !string.IsNullOrEmpty(g.Platform));
+        }
+
+        public void OnSelected(IGame selectedGame)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            getplataforma = selectedGame.Platform;
+            getgame = selectedGame.Title;
+            WebClient webClient = new WebClient();
+            string fullpath = selectedGame.ApplicationPath;
+            var romfile = Path.GetFileNameWithoutExtension(fullpath);
+            string dest = Path.Combine(path, "videos", getplataforma);
+            var client = new WebClient();
+            string url = progetto + "/" + romfile.ToLower() + ".mp4";
+            Progetto.downloadFile(url, dest + @"\" + romfile + ".mp4");
+        }
+
+        public void OnSelected(IGame[] selectedGames)
+        {
+            foreach (var item in selectedGames)
+            {
+                string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                getplataforma = item.Platform;
+                getgame = item.Title;
+                WebClient webClient = new WebClient();
+                string fullpath = item.ApplicationPath;
+                var romfile = Path.GetFileNameWithoutExtension(fullpath);
+                string dest = Path.Combine(path, "videos", getplataforma);
+                var client = new WebClient();
+                string url = progetto + "/" + romfile.ToLower() + ".mp4";
+                Progetto.downloadFile(url, dest + @"\" + romfile + ".mp4");
+            }
+            MessageBox.Show("Downloaded");
+        }
+    }
+}
---
+++ MameVideoScraper/Progetto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace MameVideoScraper
+{
+    class Progetto
+    {
+        public static void downloadFile(string url, string dest)
+        {
+            using (var client = new WebClient())
+            {
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                client.DownloadFile(url, dest);
+            }
+        }
+    }
+}
